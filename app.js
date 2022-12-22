39211e2fabe5608abbc118e6008ee3cf3f98a466
Hanfie Vandanu <hanfiev@gmail.com>
2022-12-22 13:59:56 +0700

    update cors config

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const port = 3000
 let corsOptions = {
     origin: 'https://incitu-project-maps.vercel.app/'
 };
-
+app.use(cors())
 var request = require('request');
-app.get('/:id', cors(corsOptions), function(req,res) {
+app.get('/:id', function(req,res) {
   var newurl = 'https://zap-api-production.herokuapp.com/projects/'+req.params.id;
   request(newurl).pipe(res);
 });
