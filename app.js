56027020840491737343d56a41ba4e51a6dcd812
Hanfie Vandanu <hanfiev@gmail.com>
2022-12-22 13:57:45 +0700

    add cors config

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const port = 3000
 
+let corsOptions = {
+    origin: 'https://incitu-project-maps.vercel.app/'
+};
+
 var request = require('request');
-app.get('/:id', function(req,res) {
+app.get('/:id', cors(corsOptions), function(req,res) {
   var newurl = 'https://zap-api-production.herokuapp.com/projects/'+req.params.id;
   request(newurl).pipe(res);
 });
